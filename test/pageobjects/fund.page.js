abd87ce1fd7d38a90f59aa911cc423da3a0bc9fe
parth-skuad <parth.hansalia@skuad.io>
2020-09-15 10:04:24 +0530

    fund page automation

--- test/pageobjects/fund.page.js
+++ test/pageobjects/fund.page.js
@@ -5,6 +5,20 @@ class Fund extends Page {
 
     get clickOnProfile() {return $('//*[@id="root"]/div/div/div/div/nav/div/div[2]/div/ul/li[3]');}
     get boostAmount() {return $('(//*[@class="form-control"])[1]');}
+    get clickOnboostButton() {return $('(//*[@class="btn btn-primary"])[3]');}
+    get getBoostAmount() {return $('(//*[@class="form-control"])[1]');}
+    get getCheckOutBoostAmount() {return $('div.d-flex.justify-content-end.col');}
+    get checkoutEmail() {return $('[name="email"]'); }
+    get checkoutName() {return $('[name="name"]'); }
+    get checkoutAddress() {return $('[name="address"]'); }
+    get checkoutPostalCode() {return $('[name="postalCode"]'); }
+    get checkoutCity() {return $('[name="city"]'); }
+    get checkoutRegion() {return $('[name="region"]'); }
+    get employeedCheckbox() {return $('(//*[@class="form-check-input"])[1]');}
+    get communicationByEmail() {return $('(//*[@class="form-check-input"])[2]');}
+    get testPageCommunicationByEmail() {return $('(//*[@class="form-check-input"])[5]');}
+    get clickPayButton() {return $('button.w-100.mb-4.mt-3.btn.btn-primary');}
+
 
 
     waitForPageLoad(selector) {
@@ -24,9 +38,39 @@ class Fund extends Page {
         this.clickOnProfile.click();  
     }
 
-    enterBoostAmount(){
+    enterBoostAmount(boostAmount){
         this.waitForPageLoad(this.boostAmount);
-        this.boostAmount.setValue('86');
+        this.boostAmount.setValue(boostAmount);
+    }
+
+    clickOnBoost(){
+        this.clickOnboostButton.click();
+    }
+
+    getBoostAmountOnFundPage(boostAmount){
+        this.getBoostAmount.getValue() == boostAmount && this.getCheckOutBoostAmount.getValue() == boostAmount;
+        browser.pause(5000); 
+    }
+
+    enterCheckoutFormInfo(email,name,address,pincode,city){
+        this.checkoutEmail.scrollIntoView();
+        browser.pause(3000);
+        this.checkoutEmail.setValue(email);
+        this.checkoutName.setValue(name);
+        this.checkoutAddress.setValue(address);
+        this.checkoutPostalCode.setValue(pincode);
+        this.checkoutCity.setValue(city);
+        this.checkoutRegion.selectByAttribute('value', 'DC');
+        if (this.employeedCheckbox.isSelected()) {
+            console.log('employed checkbox is selected')
+          } else {
+            this.employeedCheckbox.click();
+          }
+        this.communicationByEmail.click();
+        this.testPageCommunicationByEmail.click();
+        this.clickPayButton.scrollIntoView();
+        browser.pause(3000);
+        this.clickPayButton.click();
         browser.pause(5000); 
     }
  
--- test/specs/fund.spec.js
+++ test/specs/fund.spec.js
@@ -1,9 +1,11 @@
 import { homePage } from "../pageobjects/home.page";
 import { fundPage } from "../pageobjects/fund.page";
 
+const boostAmount = 86;
+
 describe("Continue with facebook", () => {
     it("Click on Continue facebook", () => {  
-        homePage.open()
+        homePage.open();
         homePage.clickContinueFacebook();
         homePage.loginFacebook('parthhansalia12@gmail.com','P@rth94293511651294');
     });
@@ -12,6 +14,14 @@ describe("Continue with facebook", () => {
   describe("Go to fund page", () => {
     it("Click on profile", () => {  
         fundPage.clickProfileButton();
-        fundPage.enterBoostAmount();
+        fundPage.enterBoostAmount(boostAmount);
+        fundPage.clickOnBoost();
+    });
+  });
+
+  describe("Fund page verification", () => {
+    it("Go to Fund page", () => {  
+        fundPage.getBoostAmountOnFundPage(boostAmount);
+        fundPage.enterCheckoutFormInfo('parth@skuad.io','parth','BTM','360490','Gujarat');
     });
   });
